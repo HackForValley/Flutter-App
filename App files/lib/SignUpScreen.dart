570c903d9e771d76d38dfc04404af3860d3e3f0d
unknown <vivek0094.cse19@chitkara.edu.in>
2020-06-16 23:13:33 +0530

    A few changes

--- App files/lib/SignUpScreen.dart
+++ App files/lib/SignUpScreen.dart
@@ -12,146 +12,154 @@ class _SignupscreenState extends State<Signupscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        Image.asset("assets/Scene-Whiteboard.png"),
-        ListView(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Image.asset("assets/circle.png"),
-            ),
-            //Padding(padding: EdgeInsets.only(top: 20)),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 50),
-              child: Text(
-                "Let's Get Started",
-                style: TextStyle(fontSize: 20),
-                textAlign: TextAlign.center,
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Image.asset("assets/Scene-Whiteboard.png"),
+          ListView(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Image.asset("assets/circle.png"),
               ),
-            ),
-            SizedBox(height: 15),
-            Card(
-              color: Color.fromRGBO(245, 245, 245, 100),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0),
-                //side: BorderSide(color: Colors.red),
-              ),
-              margin: EdgeInsets.symmetric(horizontal: 50),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: TextField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.grey,
-                    border: InputBorder.none,
-                    icon: Icon(
-                      Icons.format_color_text,
-                      // color: Colors.white,
-                    ),
-                    hintText: 'Name',
-                  ),
+              //Padding(padding: EdgeInsets.only(top: 20)),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 50),
+                child: Text(
+                  "Let's Get Started",
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Card(
-              color: Color.fromRGBO(245, 245, 245, 100),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0),
-                //side: BorderSide(color: Colors.red),
-              ),
-              margin: EdgeInsets.symmetric(horizontal: 50),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: TextField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.grey,
-                    border: InputBorder.none,
-                    icon: Icon(
-                      Icons.email,
-                      // color: Colors.white,
-                    ),
-                    hintText: 'Email Address',
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Card(
-              color: Color.fromRGBO(245, 245, 245, 100),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0),
-                //side: BorderSide(color: Colors.red),
-              ),
-              margin: EdgeInsets.symmetric(horizontal: 50),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: TextField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.grey,
-                    border: InputBorder.none,
-                    icon: Icon(
-                      Icons.lock,
-                      // color: Colors.white,
-                    ),
-                    hintText: 'Password',
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Card(
-              color: Color.fromRGBO(255, 255, 255, 100),
-              elevation: 0,
-              margin: EdgeInsets.symmetric(horizontal: 65),
-              child: FlatButton(
-                onPressed: () {
-                  //TODO: Add a way to Sign Uo
-                },
+              SizedBox(height: 15),
+              Card(
+                color: Color.fromRGBO(245, 245, 245, 100),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(50.0),
                   //side: BorderSide(color: Colors.red),
                 ),
+                margin: EdgeInsets.symmetric(horizontal: 50),
                 child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 13),
-                  child: Text(
-                    'Sign up'.toUpperCase(),
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey,
+                      border: InputBorder.none,
+                      icon: Icon(
+                        Icons.format_color_text,
+                        // color: Colors.white,
+                      ),
+                      hintText: 'Name',
                     ),
+                    textInputAction: TextInputAction.next,
+                    onSubmitted: (value) => FocusScope.of(context).nextFocus(),
                   ),
                 ),
-                color: Color.fromRGBO(49, 70, 240, 100),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 5),
-              child: Container(
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (_) {
-                      return LoginScreen();
-                    }));
+              SizedBox(height: 10),
+              Card(
+                color: Color.fromRGBO(245, 245, 245, 100),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                  //side: BorderSide(color: Colors.red),
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 50),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey,
+                      border: InputBorder.none,
+                      icon: Icon(
+                        Icons.email,
+                        // color: Colors.white,
+                      ),
+                      hintText: 'Email Address',
+                    ),
+                    textInputAction: TextInputAction.next,
+                    onSubmitted: (value) => FocusScope.of(context).nextFocus(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Card(
+                color: Color.fromRGBO(245, 245, 245, 100),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                  //side: BorderSide(color: Colors.red),
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 50),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey,
+                      border: InputBorder.none,
+                      icon: Icon(
+                        Icons.lock,
+                        // color: Colors.white,
+                      ),
+                      hintText: 'Password',
+                    ),
+                    obscureText: true,
+                    textInputAction: TextInputAction.next,
+                    onSubmitted: (value) => FocusScope.of(context).nextFocus(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Card(
+                color: Color.fromRGBO(255, 255, 255, 100),
+                elevation: 0,
+                margin: EdgeInsets.symmetric(horizontal: 65),
+                child: FlatButton(
+                  onPressed: () {
+                    //TODO: Add a way to Sign Uo
                   },
-                  child: Center(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                    //side: BorderSide(color: Colors.red),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 60, vertical: 13),
                     child: Text(
-                      "Already have an account? Log in.",
+                      'Sign up'.toUpperCase(),
                       style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.indigoAccent,
-                        fontWeight: FontWeight.normal,
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  color: Color.fromRGBO(49, 70, 240, 100),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: Container(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (_) {
+                        return LoginScreen();
+                      }));
+                    },
+                    child: Center(
+                      child: Text(
+                        "Already have an account? Log in.",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.indigoAccent,
+                          fontWeight: FontWeight.normal,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ],
-    ));
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- App files/lib/main.dart
+++ App files/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'LoginScreen.dart';
 import 'SignUpScreen.dart';
 
-
 //import 'package:dotted_border/dotted_border.dart';
 void main() {
   runApp(MyApp());
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(/*title: 'Flutter Demo Home Page'*/),
+      home: MyHomePage(),
     );
   }
 }
@@ -33,81 +32,82 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        Container(
-          // width: MediaQuery.of(context).size.width,
-          // height: MediaQuery.of(context).size.height*0.7,
-          decoration: BoxDecoration(
-            color: Colors.white12,
-            image: DecorationImage(
-                image: AssetImage("assets/Scene-Whiteboard.png"),
-                fit: BoxFit.cover),
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(
+            // width: MediaQuery.of(context).size.width,
+            // height: MediaQuery.of(context).size.height*0.7,
+            decoration: BoxDecoration(
+              color: Colors.white12,
+              image: DecorationImage(
+                  image: AssetImage("assets/Scene-Whiteboard.png"),
+                  fit: BoxFit.cover),
+            ),
           ),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset("assets/circle.png"),
-            SizedBox(height: 10),
-            Text("Where hackers meet hobbyists"),
-            Padding(
-              padding: EdgeInsets.only(top: 100),
-              child: FlatButton(
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("assets/circle.png"),
+              SizedBox(height: 10),
+              Text("Where hackers meet hobbyists"),
+              Padding(
+                padding: EdgeInsets.only(top: 100),
+                child: FlatButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (_) {
+                      return LoginScreen();
+                    }));
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                    //side: BorderSide(color: Colors.red),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 60, vertical: 13),
+                    child: Text(
+                      'Login'.toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  color: Color.fromRGBO(49, 70, 240, 100),
+                ),
+              ),
+              SizedBox(height: 10),
+              FlatButton(
                 onPressed: () {
                   Navigator.push(context, MaterialPageRoute(builder: (_) {
-                    return LoginScreen();
+                    return Signupscreen();
                   }));
                 },
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
-                    //side: BorderSide(color: Colors.red),
-                    ),
+                  borderRadius: BorderRadius.circular(50.0),
+                  side: BorderSide(
+                    color: Color.fromRGBO(49, 70, 240, 100),
+                  ),
+                ),
                 child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 13),
+                  padding: EdgeInsets.symmetric(horizontal: 54, vertical: 13),
                   child: Text(
-                    'Login'.toUpperCase(),
+                    'Signup'.toUpperCase(),
                     style: TextStyle(
                       fontSize: 20,
-                      color: Colors.white,
+                      color: Color.fromRGBO(49, 70, 240, 100),
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                color: Color.fromRGBO(49, 70, 240, 100),
-                
               ),
-            ),
-            SizedBox(height: 10),
-            FlatButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (_) {
-                  return Signupscreen();
-                }));
-              },
-              shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
-                    side: BorderSide(color: Color.fromRGBO(49, 70, 240, 100),),
-                    ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 54, vertical: 13),
-                child: Text(
-                  'Signup'.toUpperCase(),
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Color.fromRGBO(49, 70, 240, 100),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 30),
-            Text("Made with ❤ by Hack For Valley Team".toUpperCase())
-          ],
-        ),
-      ],
-    ));
+              SizedBox(height: 50),
+              Text("Made with ❤ by Hack For Valley Team".toUpperCase())
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
-
