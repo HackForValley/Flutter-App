9478622cedf11aa4176881ab47f3121d07bacb12
Yashank18 <yashank7696@gmail.com>
2020-06-16 22:46:02 +0530

    Text field ad Scroll error sorted

--- App files/lib/Auth/Login.dart
+++ App files/lib/Auth/Login.dart
@@ -23,114 +23,117 @@ class _LoginScreenState extends State<LoginScreen> {
             image: DecorationImage(image:AssetImage("assets/Scene-Whiteboard.png"),fit: BoxFit.cover),
           ),
           ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                 Container( 
-                 child:Image.asset("assets/circle.png"),
-                ),
-                Padding(padding: EdgeInsets.only(top:20)),
-                Container(
-                  child: Text("WELCOME,Back!",style:TextStyle(fontSize: 20),),
-                ),
-                Padding(
-                padding: EdgeInsets.only(top:80),
-                child: Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width/1.5,
-                    decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50)
-                      )
-                    ),
-                    child:TextField(
-                       decoration: InputDecoration(
-                         fillColor: Colors.grey,
-                        border: InputBorder.none,
-                        icon: Padding(
-                          padding: const EdgeInsets.only(left:10.0),
-                          child: Icon(Icons.email,
-                           // color: Colors.white,
+            SingleChildScrollView(
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                   Container( 
+                   child:Image.asset("assets/circle.png"),
+                  ),
+                  Padding(padding: EdgeInsets.only(top:20)),
+                  Container(
+                    child: Text("WELCOME BACK!",style:TextStyle(fontSize: 20),),
+                  ),
+                  Padding(
+                  padding: EdgeInsets.only(top:80),
+                  child: Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width/1.5,
+                      decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50)
+                        )
+                      ),
+                      child:TextField(
+                         decoration: InputDecoration(
+                           fillColor: Colors.grey,
+                          border: InputBorder.none,
+                          icon: Padding(
+                            padding: const EdgeInsets.only(left:10.0),
+                            child: Icon(Icons.email,
+                             // color: Colors.white,
+                            ),
                           ),
+                          hintText: 'Email Address',
                         ),
-                        hintText: 'Email Address',
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                padding: EdgeInsets.only(top:5),
-                child: Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width/1.5,
-                     //color: Colors.black12,
-                    decoration: BoxDecoration(
-                     color: Colors.black12,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50)
-                      )
-                    ),
-                    child:TextField(
-                       decoration: InputDecoration(
-                         fillColor: Colors.grey,
-                        border: InputBorder.none,
-                        icon: Padding(
-                          padding: const EdgeInsets.only(left:10.0),
-                          child: Icon(Icons.lock,
-                           // color: Colors.white,
+                  Padding(
+                  padding: EdgeInsets.only(top:5),
+                  child: Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width/1.5,
+                       //color: Colors.black12,
+                      decoration: BoxDecoration(
+                       color: Colors.black12,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50)
+                        )
+                      ),
+                      child:TextField(
+                        obscureText: true,
+                         decoration: InputDecoration(
+                           fillColor: Colors.grey,
+                          border: InputBorder.none,
+                          icon: Padding(
+                            padding: const EdgeInsets.only(left:10.0),
+                            child: Icon(Icons.lock,
+                             // color: Colors.white,
+                            ),
                           ),
+                          hintText: 'Password',
                         ),
-                        hintText: 'Password',
                       ),
                     ),
                   ),
-                ),
-                
-                  Padding(                 
-                padding: EdgeInsets.only(top: 10),
-                child:GestureDetector(
-                child:Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width/1.5,
-                    decoration: BoxDecoration(
-                      color: Colors.indigo,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50)
-                      ),
-                    ),
-                    child:Center(
-                      child: Text('Login'.toUpperCase(),
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+                  
+                    Padding(                 
+                  padding: EdgeInsets.only(top: 10),
+                  child:GestureDetector(
+                  child:Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width/1.5,
+                      decoration: BoxDecoration(
+                        color: Colors.indigo,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50)
                         ),
                       ),
-                    ),
-                  ), 
-                ),
-                ),
-                Padding(padding:EdgeInsets.only(top:5),
-                child: Container(
-                   child:GestureDetector(
-                      onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (_) {
-                    return Signupscreen();
-                    }));
-                    },
-                    child: Center(
-                      child: Text("or Create an account",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.indigoAccent,
-                          fontWeight: FontWeight.normal,
+                      child:Center(
+                        child: Text('Login'.toUpperCase(),
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                    ),
-                ),), 
-              ],
+                    ), 
+                  ),
+                  ),
+                  Padding(padding:EdgeInsets.only(top:15),
+                  child: Container(
+                     child:GestureDetector(
+                        onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (_) {
+                      return Signupscreen();
+                      }));
+                      },
+                      child: Center(
+                        child: Text("or Create an account",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.indigoAccent,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      ),
+                  ),), 
+                ],
+              ),
             ),
          
         ],
--- App files/lib/Auth/Signup.dart
+++ App files/lib/Auth/Signup.dart
@@ -23,131 +23,143 @@ class _SignupscreenState extends State<Signupscreen> {
             image: DecorationImage(image:AssetImage("assets/Scene-Whiteboard.png"),fit: BoxFit.cover),
           ),
           ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                 Container( 
-                 child:Image.asset("assets/circle.png"),
-                ),
-                Padding(padding: EdgeInsets.only(top:20)),
-                Container(
-                  child: Text("Let's Get Started",style:TextStyle(fontSize: 20),),
-                ),
-                 Padding(                 
-                padding: EdgeInsets.only(top: 20),
-                child:Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width/1.5,
-                    decoration: BoxDecoration(
+            SingleChildScrollView(
+                          child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                   Container( 
+                   child:Image.asset("assets/circle.png"),
+                  ),
+                  Padding(padding: EdgeInsets.only(top:20)),
+                  Container(
+                    child: Text("Let's Get Started",style:TextStyle(fontSize: 20),),
+                  ),
+                   Padding(                 
+                  padding: EdgeInsets.only(top: 20),
+                  child:Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width/1.5,
+                      decoration: BoxDecoration(
+                         color: Colors.black12,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50)
+                        )
+                      ),
+                      child:TextField(
+                         decoration: InputDecoration(
+                           fillColor: Colors.grey,
+                          border: InputBorder.none,
+                          icon: Padding(
+                            padding: const EdgeInsets.only(left:10.0),
+                            child: Icon(Icons.format_color_text,
+                              //color: Colors.grey,
+                            ),
+                          ),
+                          hintText: 'Name',
+                        ),
+                      ),
+                    ), 
+                  ),
+                  Padding(
+                  padding: EdgeInsets.only(top:5),
+                  child: Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width/1.5,
+                      decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50)
+                        )
+                      ),
+                      child:TextField(
+                         decoration: InputDecoration(
+                           fillColor: Colors.grey,
+                          border: InputBorder.none,
+                          icon: Padding(
+                            padding: const EdgeInsets.only(left:10.0),
+                            child: Icon(Icons.email,
+                         //   color: Colors.white,
+                            ),
+                          ),
+                          hintText: 'Email Address',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                  padding: EdgeInsets.only(top:5),
+                  child: Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width/1.5,
+                      decoration: BoxDecoration(
                        color: Colors.black12,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50)
-                      )
-                    ),
-                    child:TextField(
-                       decoration: InputDecoration(
-                         fillColor: Colors.grey,
-                        border: InputBorder.none,
-                        icon: Icon(Icons.format_color_text,
-                          //color: Colors.grey,
-                        ),
-                        hintText: 'Name',
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50)
+                        )
                       ),
-                    ),
-                  ), 
-                ),
-                Padding(
-                padding: EdgeInsets.only(top:5),
-                child: Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width/1.5,
-                    decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50)
-                      )
-                    ),
-                    child:TextField(
-                       decoration: InputDecoration(
-                         fillColor: Colors.grey,
-                        border: InputBorder.none,
-                        icon: Icon(Icons.email,
-                       //   color: Colors.white,
+                      child:TextField(
+                        obscureText: true,
+                         decoration: InputDecoration(
+                           fillColor: Colors.grey,
+                          border: InputBorder.none,
+                          icon: Padding(
+                            padding: const EdgeInsets.only(left:10.0),
+                            child: Icon(Icons.lock,
+                              //color: Colors.white,
+                            ),
+                          ),
+                          hintText: 'Password',
                         ),
-                        hintText: 'Email Address',
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                padding: EdgeInsets.only(top:5),
-                child: Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width/1.5,
-                    decoration: BoxDecoration(
-                     color: Colors.black12,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50)
-                      )
-                    ),
-                    child:TextField(
-                       decoration: InputDecoration(
-                         fillColor: Colors.grey,
-                        border: InputBorder.none,
-                        icon: Icon(Icons.lock,
-                          //color: Colors.white,
+                  
+                    Padding(                 
+                  padding: EdgeInsets.only(top: 10),
+                  child:GestureDetector(
+                  child:Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width/1.5,
+                      decoration: BoxDecoration(
+                        color: Colors.indigo,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50)
                         ),
-                        hintText: 'Password',
                       ),
-                    ),
+                      child:Center(
+                        child: Text('Signup'.toUpperCase(),
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ), 
                   ),
-                ),
-                
-                  Padding(                 
-                padding: EdgeInsets.only(top: 10),
-                child:GestureDetector(
-                child:Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width/1.5,
-                    decoration: BoxDecoration(
-                      color: Colors.indigo,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50)
-                      ),
-                    ),
-                    child:Center(
-                      child: Text('Signup'.toUpperCase(),
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+                  ),
+                  Padding(padding:EdgeInsets.only(top:15),
+                  child: Container(
+                     child:GestureDetector(
+                         onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (_) {
+                      return LoginScreen();
+                      }));
+                      },
+                      child: Center(
+                        child: Text("Already have an account? Log in.",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.indigoAccent,
+                            fontWeight: FontWeight.normal,
+                          ),
                         ),
                       ),
-                    ),
+                      ),
+                  ),
                   ), 
-                ),
-                ),
-                Padding(padding:EdgeInsets.only(top:5),
-                child: Container(
-                   child:GestureDetector(
-                       onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (_) {
-                    return LoginScreen();
-                    }));
-                    },
-                    child: Center(
-                      child: Text("Already have an account? Log in.",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.indigoAccent,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                    ),
-                    ),
-                ),
-                ), 
-              ],
+                ],
+              ),
             ),
          
         ],
